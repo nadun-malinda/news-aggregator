2798d90012f0dc8c8127c08e6163f3dbdb286ad5
Nadun Malinda <bknandun@gmail.com>
2024-12-31 15:30:42 +0100

    add: redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ function App() {
   return (
     <div className="max-w-screen-lg mx-auto p-4">
       <p className="text-lg">News Aggregator</p>
-      <Input type="text" placeholder="Search" />
-      <Button>Click me</Button>
+      <div className="flex gap-2">
+        <Input type="text" placeholder="Search" />
+        <Button>Click me</Button>
+      </div>
     </div>
   );
 }
